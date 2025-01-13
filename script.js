d70a3e9638d11db12889ef02a1419b7956de7413
kristyjanzer <kristy.net.ua@mail.ru>
2025-01-13 18:40:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,15 @@
 window.addEventListener('popstate', function(event) {
   // Событие popstate запускается каждый раз при изменении текущей записи истории.
 
-  var r = window.location.href = 'https://vk.com/';
+  var r = window.location.href = 'https:/youtube.com/';
+  console.log(r);
 
   if (r == true) {
-    console.log(r);
       // Кнопка обратного звонка программно активируется после подтверждения пользователя.
-      history.back();
+      // history.back();
+      window.location.href = 'https://vk.com/';
       // Раскомментируйте строку ниже, чтобы перейти на предыдущую страницу.
-      window.location = document.referrer // Примечание: IE11 не поддерживает это.
+      // window.location = document.referrer // Примечание: IE11 не поддерживает это.
   } else {
       // Остаться на текущей странице.
       history.pushState(null, null, window.location.pathname);
