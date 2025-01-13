631d86ca47726a5113c4f9aa2c092021b0780c6c
kristyjanzer <kristy.net.ua@mail.ru>
2025-01-13 18:24:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// window.addEventListener(
+//   'visibilitychange', 
+//   function() { 
+//       if (document.hidden === true) return;
+//       console.log('Вернулся');
+//   }, 
+//   false
+// );
+
+// window.onhashchange = function() {
+//   console.log('Вернулся');
+// }
+
+window.addEventListener('popstate', function(event) {
+  // Событие popstate запускается каждый раз при изменении текущей записи истории.
+
+  var r = window.location.href = 'https://vk.com/';
+
+  if (r == true) {
+      // Кнопка обратного звонка программно активируется после подтверждения пользователя.
+      history.back();
+      // Раскомментируйте строку ниже, чтобы перейти на предыдущую страницу.
+      // window.location = document.referrer // Примечание: IE11 не поддерживает это.
+  } else {
+      // Остаться на текущей странице.
+      history.pushState(null, null, window.location.pathname);
+  }
+
+  history.pushState(null, null, window.location.pathname);
+
+}, false);
