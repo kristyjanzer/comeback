7b6521b85eb00df25925c3b25848f043fec8820c
kristyjanzer <kristy.net.ua@mail.ru>
2025-01-14 16:00:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,40 +11,25 @@
 //   console.log('Вернулся');
 // }
 
-// window.addEventListener('popstate', function(event) {
-//   // Событие popstate запускается каждый раз при изменении текущей записи истории.
-
-//   // var r = confirm("You pressed a Back button! Are you sure?!");
-
-//   if (event == true) {
-//     window.location.href="https://vk.com/"
-//     console.log(window.location.replace);
-//       // Кнопка обратного звонка программно активируется после подтверждения пользователя.
-//       // history.back();
-//       // Раскомментируйте строку ниже, чтобы перейти на предыдущую страницу.
-//       // window.location = document.referrer // Примечание: IE11 не поддерживает это.
-//   } else {
-//       // Остаться на текущей странице.
-//       history.pushState(null, null, window.location.pathname);
-//     console.log(history.pushState(null, null, window.location.pathname));
-//   }
-
-//   history.pushState(null, null, window.location.pathname);
-
-// }, false);
-
-function pushStateWithSource(state, title, url, source) {
-  history.pushState({ ...state, source }, title, url);
-}
-
-// Задаём направление как 'назад' или 'вперёд'
-// Это можно сравнить с тем, как если бы вы дали пользователю компас
-pushStateWithSource({}, '', 'nextpage.html', 'back');
-
-// Отслеживаем событие popstate
 window.addEventListener('popstate', function(event) {
-  // По событию можем определить, в каком направлении двигался пользователь
-  if(event.state?.source === 'back') {
-    console.log(`Переход ${event.state.source}`); // Выведет 'Переход назад' или 'Переход вперёд'
+  // Событие popstate запускается каждый раз при изменении текущей записи истории.
+
+  // var r = confirm("You pressed a Back button! Are you sure?!");
+
+  if(event.state){
+     console.log("User clicked the browser buttons. Detected using popstate event.");
+    // window.location.href="https://vk.com/"
+      // Кнопка обратного звонка программно активируется после подтверждения пользователя.
+      // history.back();
+      // Раскомментируйте строку ниже, чтобы перейти на предыдущую страницу.
+      // window.location = document.referrer // Примечание: IE11 не поддерживает это.
+  } else {
+      // Остаться на текущей странице.
+      history.pushState(null, null, window.location.pathname);
+    console.log('re');
   }
-});
+
+  history.pushState(null, null, window.location.pathname);
+
+}, false);
+
